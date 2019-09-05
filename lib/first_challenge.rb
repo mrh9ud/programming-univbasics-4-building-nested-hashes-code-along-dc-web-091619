670e2421ee861ba916a0af5c350f0a6fad7aa4a1
Matthew Heavner <mrh9ud@virginia.edu>
2019-09-05 18:29:58 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,18 +1,8 @@
 def first_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
+    :montague => {},
+    :capulet => {}
+  }
 end
 
 
