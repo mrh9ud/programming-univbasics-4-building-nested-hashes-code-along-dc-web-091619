9990918ea414448d01ad54f87dca1ea6cd5d140c
Matthew Heavner <mrh9ud@virginia.edu>
2019-09-05 18:10:21 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,9 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {:patriarch => {:name => "Lord Montague", 
+         :age => "53"
+         }, 
+        :matriarch => {:name => "Lady Montague", :age => "54"}, :hero => {:name => "Romeo", :age => 15}},
+     :capulet => {}
+   }
 end
